4922ea231c3dcc75571ab17388a0e4f200f636bb
gabriel.alonso <gabriel.alonso@ross.com.ar>
2023-06-14 22:16:43 -0300

    avances hasta clase 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+//import { ContadorComponent } from './contador/contador/contador.component'; //remover esta linea
+
+// Tarea;
+// crear un modulo llamado contadorModule
+// en el cual como no hay ngif ni ngfor no hay que imporytar nada
+// hay que hacer declaraciones y exportaciones
+// tengo que agregar en los imports aqui en este archivo y asegurar que la aplicacion funcione
+
+import { HeroesModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    //ContadorComponent //remover esta linea
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule, // importar aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { startWith } from 'rxjs';
 
 @Component({
   selector: 'app-listado',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
+  heroes: string[] = ['Spiderman','Ironman','Hulk','Thor','Capitan America'];
+
+  heroeBorrado : string ='';
+
+  borrarHeroe() {
+
+    console.log('borrando...');
+    //this.heroes.pop(); //este lo hice yo solo y borra desde el ultimo para arriba
+    //this.heroes = []; // esto borra todos los items de una sola vez
+    this.heroeBorrado = this.heroes.shift() || ''; // remueve el primer elemento del arreglo y lo retorna // 
+
+  
+    
+  }
 }
