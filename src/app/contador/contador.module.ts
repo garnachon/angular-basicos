733148e2b4945d3edf19ac7ebef39b2af241b2f1
gabriel.alonso <gabriel.alonso@ross.com.ar>
2023-06-14 22:20:26 -0300

    avances hasta clase 20

--- src/app/contador/contador.module.ts
+++ src/app/contador/contador.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ContadorComponent } from './contador/contador.component';
-import { CommonModule } from '@angular/common';
+
 
 @NgModule({
     declarations: [
